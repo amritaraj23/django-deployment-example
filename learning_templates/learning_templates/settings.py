5803f2f74d413c5a016e924db4af713f73874efd
raj503 <amrita.raj@kpn.com>
2021-03-19 16:33:46 +0100

    first commit

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = str(Path(__file__).resolve().parent.parent)
-TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(str(BASE_DIR),'templates')
 
 
 # Quick-start development settings - unsuitable for production
